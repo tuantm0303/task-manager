c2b331c63a8a2a394578cee5fb07262d91deee6c
tuantm0303 <minhtuan0330@gmail.com>
2024-01-09 16:57:51 +0700

    feat: delete job

--- src/components/ItemJob.js
+++ src/components/ItemJob.js
@@ -1,10 +1,16 @@
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 import './ItemJob.scss';
 
-const ItemJob = ({ title, desc }) => {
+const ItemJob = ({ title, desc, id, handleDelete }) => {
   return (
     <div className="wlgwnxkuca_item">
       <div className="cnnpomcvrg_title">{title}</div>
       <div className="idzbwkwnte_desc">{desc}</div>
+      <div className="henmcposup_btn">
+        <CiEdit className="bjvnitqkwa" />
+        <MdDeleteOutline onClick={() => handleDelete(id)} className="bjvnitqkwa" />
+      </div>
     </div>
   );
 };
--- src/components/ListJob.js
+++ src/components/ListJob.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import ItemJob from './ItemJob';
 import './ListJob.scss';
 
 const List = ({ jobStatus }) => {
-  const jobs = JSON.parse(localStorage.getItem('jobs'))
+  const [jobs, setJobs] = useState(() => {
+    const getLocalState = JSON.parse(localStorage.getItem('jobs'))
+    return getLocalState || []
+  })
 
+  const handleDelete = (id) => {
+    const newJobs = jobs.filter((_, index) => index !== id)
+    setJobs(newJobs)
+    localStorage.setItem('jobs', JSON.stringify(newJobs))
+  }
   return (
     <div className="fjtekxvnvt_list">
       <h1 className="fdetbnjxio_col">{jobStatus}</h1>
@@ -11,8 +20,10 @@ const List = ({ jobStatus }) => {
         return (
           <ItemJob
             key={index}
+            id={index}
             title={job?.title}
             desc={job?.desc}
+            handleDelete={handleDelete}
           />
         )
       })}
